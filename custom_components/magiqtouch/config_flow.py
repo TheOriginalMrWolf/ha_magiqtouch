fccc2fdb72d0943b8bf122c5729b517860c16560
The Original Mr Wolf <22119583+TheOriginalMrWolf@users.noreply.github.com>
2025-07-01 20:25:46 +1000

    Hopefully allow integration entry CONFIGURE to work

--- custom_components/magiqtouch/config_flow.py
+++ custom_components/magiqtouch/config_flow.py
@@ -93,14 +93,14 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):  # type: ignore[call
 
 
 class OptionsFlowHandler(config_entries.OptionsFlow):
-    # def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-    #    """Initialize options flow."""
-    #    self.entry = config_entry
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+       """Initialize options flow."""
+       self.entry = config_entry
 
     async def async_step_init(self, user_input: dict[str, Any] | None = None) -> Dict[str, Any]:
         """Manage the options."""
         if user_input is not None:
-            return self.async_create_entry(title="Settings", options=user_input)
+            return self.async_create_entry(title="Settings", data=user_input)
 
         return self.async_show_form(
             step_id="init",
